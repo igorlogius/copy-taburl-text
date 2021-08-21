88b59491193e1d3d86515b239caa20cc09787722
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-22 00:38:58 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,16 @@
+
+//const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+
+browser.pageAction.onClicked.addListener((tab) => {
+	navigator.clipboard.writeText(tab.url);
+
+	browser.notifications.create(extname, {
+		"type": "basic",
+		//"iconUrl": browser.runtime.getURL("icon.png"),
+		"title": extname,
+		"message":  'copied url to clipboard' 
+	});
+}); 
+
